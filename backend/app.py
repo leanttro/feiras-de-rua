74902a809e5b657180214c3d8a9b90d856405b17
SeuNome <leandroandrade020797@gmail.com>
2025-10-08 16:20:50 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -108,6 +108,6 @@ def get_feiras():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-
+    port = int(os.environ.get("PORT", 5000))  # fallback local
+    app.run(host="0.0.0.0", port=port, debug=True)
     
